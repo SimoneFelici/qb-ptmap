ae57ac7c506b4f2b9a26390c3b3f3a6ae9f3ef84
SimoneFelici <felicisimone9@gmail.com>
2025-08-15 14:43:08 +0200

    added configuration file

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import subprocess
 import re
 import sys
 from qbittorrentapi import Client
-
-QBIT_HOST = 'localhost:8080'
-QBIT_USER = 'admin'
-QBIT_PASS = 'adminadmin'
-NATPMP_COMMAND = "while true ; do date ; natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 || { echo -e 'ERROR with natpmpc command \a' ; break ; } ; sleep 45 ; done"
+import config
 
 current_qbit_port = 0
 
@@ -17,7 +13,7 @@ def set_qbittorrent_port(port_number):
 
     print(f"Found port: {port_number}.")
     try:
-        client = Client(host=QBIT_HOST, username=QBIT_USER, password=QBIT_PASS)
+        client = Client(host=config.QBIT_HOST, username=config.QBIT_USER, password=config.QBIT_PASS)
         client.auth_log_in()
         client.app.set_preferences(prefs={'listen_port': port_number})
         
@@ -29,8 +25,7 @@ def set_qbittorrent_port(port_number):
 
 def run_and_monitor():
     port_pattern = re.compile(r"Mapped public port (\d+) protocol (UDP|TCP)")
-    process = subprocess.Popen(NATPMP_COMMAND, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
-
+    process = subprocess.Popen(config.NATPMP_COMMAND, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
     for line in iter(process.stdout.readline, ''):
         line = line.strip()
         print(line)
@@ -43,7 +38,7 @@ def run_and_monitor():
     process.stdout.close()
     return_code = process.wait()
     if return_code:
-        raise subprocess.CalledProcessError(return_code, NATPMP_COMMAND)
+        raise subprocess.CalledProcessError(return_code, config.NATPMP_COMMAND)
 
 if __name__ == '__main__':
     try:
